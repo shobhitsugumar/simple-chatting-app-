03b3412eddc8d79b83b5a96cea4653177067349b
Shobith S <shobithsugumar@gmail.com>
2024-02-27 23:44:46 +0530

    chat app

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const { Server } = require("socket.io");
 
 const server = http.createServer(app);
+const io = new Server(server);
 const port = 3000;
+
+//socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (msg) => {
+    io.emit("usermessage", msg);
+  });
+});
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
